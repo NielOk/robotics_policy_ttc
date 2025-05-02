af1fdf25c54b61e5065ebc73ab25c87f547c3dc7
Niel Ok <nielok@stanford.edu>
2025-05-01 23:57:21 -0700

    vision policy environment demo

---
+++ push_t_vision_policy_experiments/vision_policy_push_t.py
@@ -0,0 +1,63 @@
+### This script is the set of dmeos for push t vision policy, and uses pretrained weights ###
+
+# diffusion policy import
+from typing import Tuple, Sequence, Dict, Union, Optional, Callable
+import numpy as np
+import math
+import torch
+import torch.nn as nn
+import torchvision
+import collections
+import zarr
+from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+from diffusers.training_utils import EMAModel
+from diffusers.optimization import get_scheduler
+from tqdm.auto import tqdm
+
+# env import
+import gym
+from gym import spaces
+import pygame
+import pymunk
+import pymunk.pygame_util
+from pymunk.space_debug_draw_options import SpaceDebugColor
+from pymunk.vec2d import Vec2d
+import shapely.geometry as sg
+import cv2
+import skimage.transform as st
+from skvideo.io import vwrite
+from IPython.display import Video
+import gdown
+import os
+
+# Local code imports
+from push_t_vision_env import *
+
+def environment_demo():
+    # 0. create env object
+    env = PushTImageEnv()
+
+    # 1. seed env for initial state.
+    # Seed 0-200 are used for the demonstration dataset.
+    env.seed(1000)
+
+    # 2. must reset before use
+    obs, info = env.reset()
+
+    # 3. 2D positional action space [0,512]
+    action = env.action_space.sample()
+
+    # 4. Standard gym step method
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    # prints and explains each dimension of the observation and action vectors
+    with np.printoptions(precision=4, suppress=True, threshold=5):
+        print("obs['image'].shape:", obs['image'].shape, "float32, [0,1]")
+        print("obs['agent_pos'].shape:", obs['agent_pos'].shape, "float32, [0,512]")
+        print("action.shape: ", action.shape, "float32, [0,512]")
+
+if __name__ == '__main__':
+
+    # Run environment demo
+    print("Running environment demo...")
+    environment_demo()
