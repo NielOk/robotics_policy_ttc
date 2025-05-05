3f2d51ba0050b2744bff25723c32f1e7923a717e
Niel Ok <nielok@stanford.edu>
2025-05-05 04:34:52 -0700

    added automatic state based 100 epoch chkpt model download

--- setup.sh
+++ setup.sh
@@ -4,26 +4,25 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 # Download dataset
-# Exit immediately on error
-set -e
-
-# Define the tag and repo
 REPO="NielOk/robotics_policy_ttc"
 TAG="dataset_v1"
-ASSET_NAME="pusht_cchi_v7_replay.zip"
+DATA_ASSET_NAME="pusht_cchi_v7_replay.zip"
+DATA_TARGET_DIR="./"
 
-# Create target directory
-TARGET_DIR="./"
-mkdir -p "$TARGET_DIR"
-
-# Download the dataset
 echo "Downloading dataset from GitHub release..."
-curl -L -o "$TARGET_DIR/$ASSET_NAME" "https://github.com/$REPO/releases/download/$TAG/$ASSET_NAME"
+curl -L -o "$DATA_TARGET_DIR/$DATA_ASSET_NAME" "https://github.com/$REPO/releases/download/$TAG/$DATA_ASSET_NAME"
 
 # Unzip the dataset
 echo "Extracting dataset..."
-unzip -q "$TARGET_DIR/$ASSET_NAME" -d "$TARGET_DIR"
-rm "$TARGET_DIR/$ASSET_NAME"
+unzip -q "$DATA_TARGET_DIR/$DATA_ASSET_NAME" -d "$DATA_TARGET_DIR"
+rm "$DATA_TARGET_DIR/$DATA_ASSET_NAME"
 
 # Done
-echo "Dataset installed at: $TARGET_DIR"
+echo "Dataset installed at: $DATA_TARGET_DIR"
+
+# Download pretrained statebased 100 epoch model
+STATE_100_EPOCH_MODEL="pusht_state_100ep.ckpt"
+STATE_100_EPOCH_MODEL_TARGET_DIR="./push_t_state_policy_experiments/"
+
+echo "Downloading 100 epoch chkpt statebased model from GitHub release..."
+curl -L -o "$STATE_100_EPOCH_MODEL_TARGET_DIR/$STATE_100_EPOCH_MODEL" "https://github.com/NielOk/robotics_policy_ttc/releases/download/$TAG/$STATE_100_EPOCH_MODEL"\
