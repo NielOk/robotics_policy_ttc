82491a5a7f7ec3205f6b83200b4add5f9b2eec59
Niel Ok <nielok@stanford.edu>
2025-05-10 15:21:57 -0700

    updated setup.sh to download updated zips

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 
 # Download dataset
 REPO="NielOk/robotics_policy_ttc"
-TAG="dataset_v1"
+TAG="dataset_and_model_v1"
 DATA_ASSET_NAME="pusht_cchi_v7_replay.zip"
 DATA_TARGET_DIR="./"
 
@@ -25,4 +25,4 @@ STATE_100_EPOCH_MODEL="pusht_state_100ep.ckpt"
 STATE_100_EPOCH_MODEL_TARGET_DIR="./push_t_state_policy_experiments/"
 
 echo "Downloading 100 epoch chkpt statebased model from GitHub release..."
-curl -L -o "$STATE_100_EPOCH_MODEL_TARGET_DIR/$STATE_100_EPOCH_MODEL" "https://github.com/NielOk/robotics_policy_ttc/releases/download/$TAG/$STATE_100_EPOCH_MODEL"\
+curl -L -o "$STATE_100_EPOCH_MODEL_TARGET_DIR/$STATE_100_EPOCH_MODEL" "https://github.com/$REPO/releases/download/$TAG/$STATE_100_EPOCH_MODEL"\
